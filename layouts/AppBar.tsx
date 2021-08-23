f8ed4ee3c8f4b1fdc1a4c2a3e635e239d1922d6a
Khoa1468 <khoalam1468@gmail.com>
2021-08-23 18:12:17 +0700

    edit AppBar.tsx part 2

--- layouts/AppBar.tsx
+++ layouts/AppBar.tsx
@@ -1,52 +1,89 @@
-import { makeStyles } from "@material-ui/core/styles";
+import Link from "next/link";
+import React from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Link from "next/link";
-import { MenuItem } from "@material-ui/core";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import Switch from "@material-ui/core/Switch";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormGroup from "@material-ui/core/FormGroup";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  })
+);
 
-export default function NavBar(): JSX.Element {
+export default function NavBar() {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
           <IconButton
-            size="medium"
             edge="start"
+            className={classes.menuButton}
             color="inherit"
             aria-label="menu"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenu}
           >
-            <MenuIcon className={classes.menuButton} />
+            <MenuIcon />
           </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={open}
+            onClose={handleClose}
+          >
+            <Link href="/" passHref>
+              <a href="/">
+                <MenuItem onClick={handleClose}>Home</MenuItem>
+              </a>
+            </Link>
+            <Link href="/about" passHref>
+              <a href="/about">
+                <MenuItem onClick={handleClose}>About</MenuItem>
+              </a>
+            </Link>
+          </Menu>
           <Typography variant="h6" className={classes.title}>
-            React App
+            Next App
           </Typography>
-          <Link href="/about" passHref>
-            <a href="/about">
-              <MenuItem>About</MenuItem>
-            </a>
-          </Link>
-          <Link href="/" passHref>
-            <a href="/">
-              <MenuItem>Home</MenuItem>
-            </a>
-          </Link>
         </Toolbar>
       </AppBar>
     </div>
