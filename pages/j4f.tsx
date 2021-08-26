9d13a0db87f1087bae696a9f5f61698e137c92ba
Khoa1468 <khoalam1468@gmail.com>
2021-08-26 12:13:53 +0700

    j4f

--- pages/j4f.tsx
+++ pages/j4f.tsx
@@ -2,16 +2,20 @@ import Template from "layouts/Template";
 import { useEffect, useCallback } from "react";
 
 export default function Test() {
-  useEffect(() => {
-    setTimeout(() => {
-      location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-    }, 1000);
-  }, []);
   return (
-    <Template className="home">
+    <Template headingTitle="J4F" className="home">
       <h1>
         <h1>J4F</h1>
       </h1>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
     </Template>
   );
 }
